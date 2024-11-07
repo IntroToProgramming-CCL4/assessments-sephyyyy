2251f9eca25fc0bf05a647c05dc072c7f474afb6
Joseph Tambis <103038052+joseph-tambis@users.noreply.github.com>
2024-11-07 19:35:44 +0400

    Update 02_exercise.py

--- Assessment 1/Assessment 1 - Programming-Skills-Portfolio - Joseph Tambis/02_exercise.py
+++ Assessment 1/Assessment 1 - Programming-Skills-Portfolio - Joseph Tambis/02_exercise.py
@@ -1,6 +1,13 @@
 # Exercise 2: Simple Sums
 
+# This program defines two integers, calculates the sum, places the result in a third variable, and then prints the final value.
+
+# I defined the value of two integer variables to 8 and 10.
 a = 8
 b = 10
+
+# This the determines the sum of a and b, then records the outcome in variable c.
 c = a+b
+
+# This then finishes the task by printing the variable 'c' which holds the sum of 'a' and 'b'.
 print(c)
