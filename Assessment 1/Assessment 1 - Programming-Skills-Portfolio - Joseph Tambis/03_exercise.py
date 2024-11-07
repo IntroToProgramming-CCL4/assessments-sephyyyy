077efa106353c02b1683e4d4387c7d96d6118f34
Joseph Tambis <103038052+joseph-tambis@users.noreply.github.com>
2024-11-07 19:35:58 +0400

    Update 03_exercise.py

--- Assessment 1/Assessment 1 - Programming-Skills-Portfolio - Joseph Tambis/03_exercise.py
+++ Assessment 1/Assessment 1 - Programming-Skills-Portfolio - Joseph Tambis/03_exercise.py
@@ -1,26 +1,24 @@
 # Exercise 3: Biography
 
-# Advanced Requirements:
+# This program asks the user for their name, hometown, and age, stores them in a dictionary, and then displays the information across three lines.
+
+# The program starts by asking for three inputs and stores them in separate variables.
 user_name = input("What is your name?  ")
 town = input("Where is your hometown?  ")
 years = input("What's your age?  ")
 
+# This part splits any multiple-word inputs in 'user_name' and 'town' into a list.
 user_name.split()
 town.split()
 
+# The dictionary 'user_info' is then created to store the inputs. 'user_name' and 'town' will be lists, while 'years' will be a string or integer.
 user_info = {'name': [user_name],'hometown': [town],'age': years}
+
+# Three new variables are created by calling values from the dictionary. 
+# 'user_name' and 'town' are then joined using {' '.join} to combine list items into a string with spaces between the words.
 name = ' '.join(user_info['name'])
 hometown = ' '.join(user_info['hometown'])
 age = user_info ['age']
-print (f"Hi {name}!")
-print (f"According to your inputs, I know that you're {age} years old and your hometown is {hometown}.")
-#print (user_info)
 
-#Basic Requirements:
-#personal_info = {'name': 'Joseph','hometown':'Cabadbaran','age':18}
-#name = personal_info ['name']
-#hometown = personal_info ['hometown']
-#age = personal_info ['age']
-#print (f"The users name is: {name}")
-#print (f"Joseph's hometown is {hometown}")
-#print (f"Joseph's is {age} years old.")
+# This line will then print the information in the dictionary on three separate lines using a single print() statement, ending the program.
+print (f"Your name is {name}\nYour hometown is {hometown}\nYou're {age} years old")
