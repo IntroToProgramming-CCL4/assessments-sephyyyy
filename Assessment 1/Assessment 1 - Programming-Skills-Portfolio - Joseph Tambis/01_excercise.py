cea13f08df7481edfcc45bb05a56360a0cb590c6
Joseph Tambis <103038052+joseph-tambis@users.noreply.github.com>
2024-11-07 19:35:28 +0400

    Update 01_excercise.py

--- Assessment 1/Assessment 1 - Programming-Skills-Portfolio - Joseph Tambis/01_excercise.py
+++ Assessment 1/Assessment 1 - Programming-Skills-Portfolio - Joseph Tambis/01_excercise.py
@@ -1,7 +1,11 @@
 # Exercise 1: Coding is Cool
 
+# This program prints the phrase 'Coding is Cool' using string concatenation.
+
+# I used the phrase's words to define three string variables.
 word1 = "Coding"
 word2 = "is"
 word3 = "Cool"
-# Using string concatenation to combine the variables and print the phrase
-print(word1,word2,word3)
+
+# This line then concatenates the variables using the + operator and adds spaces between each word.
+print(word1+' '+word2+' '+word3)
